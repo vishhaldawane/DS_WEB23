4babf0f4a7b9707e7689de278a91d14dcef52561
vishhaldawane <vishalviniing@gmail.com>
2023-08-01 15:19:53 +0530

    compare

---
+++ MyWebProject/public/ValidateDate.js
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+ 
+ 
+ function validateBirthdate() {
+ 
+ 	today = new Date();
+ 	
+ 
+	var dob = document.getElementById("udob").value;
+	
+	console.log('birthdate : '+dob);
+	console.log('today     : '+today);
+	
+	return false;
+ }
+ 
+ 
